c5c15c426f35c5dd4b241fe1a7752dc4809baa6b
makoto <94143122+Sergeywws13@users.noreply.github.com>
2024-09-09 10:23:05 +0400

    sqlaclemy

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from aiogram import Bot, Dispatcher, types
 from dotenv import find_dotenv, load_dotenv # type: ignore
 from common.bot_cmds_list import private
+from database.models import async_main
 
 load_dotenv(find_dotenv())
 
@@ -12,16 +13,24 @@ from handlers import user_open
 from handlers import user_group
 from handlers import admin_private
 
-bot = Bot(token=os.getenv('TOKEN'))
-bot.my_admins_list = []
 
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
-dp.include_router(user_open.user_opened)
-dp.include_router(user_group.user_group_router)
-dp.include_router(admin_private.admin_router)
+
+# bot.my_admins_list = []
 
 
 async def main():
+    try:
+        await async_main()
+    except Exception as e:
+        logging.error(f"Database error: {e}")
+
+
+    dp.include_router(user_open.user_opened)
+    dp.include_router(user_group.user_group_router)
+    dp.include_router(admin_private.admin_router)
+
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot)
--- database/models.py
+++ database/models.py
@@ -1,17 +1,42 @@
-from sqlalchemy import DateTime, String, Text, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import os
+from dotenv import find_dotenv, load_dotenv
 
 
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-class Base(DeclarativeBase):
-    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
-    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+load_dotenv(find_dotenv())
+
+engine = create_async_engine(os.getenv('SQLALCHEMY_URL'), echo=True)
+
+async_session = async_sessionmaker(engine)
 
 
-class Create():
-    __tablename__ = 'create'
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    Name: Mapped[str] = mapped_column(String(150), nullable=False)
-    description: Mapped[str] = mapped_column(Text)
-    image: Mapped[str] = mapped_column(String(150))
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger)
+
+    orders = relationship("Order", back_populates="user")
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    phone: Mapped[str] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user = relationship("User", back_populates="orders")
+
+async def async_main():
+    async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
